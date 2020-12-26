6d733b29bbfb3864adc1a3df474af60c2891a713
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2020-12-26 20:19:43 +0000

    moving the map.pgm and map.yaml to the world folder

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/ahmed__world.world " &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/ahmed__world.world" &
 sleep 5
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch  map_file:=/home/workspace/catkin_ws/src/map/map.yaml " &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch  map_file:=/home/workspace/catkin_ws/src/world/map.yaml" &
 sleep 5
-xterm -e " rosrun rviz rviz -d ~/catkin_ws/src/rvizConfig/home_service.rviz " &
+xterm -e " rosrun rviz rviz -d ~/catkin_ws/src/rvizConfig/home_service.rviz" &
 sleep 15
 xterm -e "rosrun add_markers add_markers " &
 sleep 5
