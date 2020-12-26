e9be828363756940c0b04011a3b31c3a894cc8a9
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2020-12-26 21:22:38 +0000

    modifing the scripts folder

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/ahmed__world.world " &
+#!/bin/bash
+
+# Build the catkin_ws
+cd $(pwd)/../..; catkin_make
+
+# Launch the nodes
+xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/ahemd__world.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 7
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch  map_file:=/home/workspace/catkin_ws/src/world/map.yaml " &
-sleep 7
-xterm -e " rosrun rviz rviz -d ~/catkin_ws/src/rvizConfig/home_service.rviz " &
+xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/world/map.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch " &
+sleep 5
+xterm  -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
-xterm -e "rosrun add_markers add_markers " &
-sleep 7
-xterm -e "rosrun pick_objects pick_objects "
+xterm  -e "source devel/setup.bash; rosrun add_markers add_markers" &
+sleep 5
+xterm  -e "source devel/setup.bash; rosrun pick_objects pick_objects"
+
 
 
 
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
-#xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/world/ahmed__world.world " &
-#xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=~/catkin_ws/src/worlds/map.world " &
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 3
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch " &
-sleep 3
-xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
-sleep 3
-xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch "
+#!/bin/bash
+
+# Build the catkin_ws
+cd $(pwd)/../..; catkin_make
+
+# Launch the nodes
+xterm  -e "source devel/setup.bash; export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/ahmed__world.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 10
+xterm  -e "source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
+sleep 5
+xterm  -e "source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e "source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch"
+
+
