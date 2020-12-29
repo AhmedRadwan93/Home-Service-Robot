247bf19be64287f81f59ffd5e6adec6862a164be
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2020-12-29 10:38:27 +0000

    modification for the homeservice  script file

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -2,9 +2,9 @@
 
 
 # Launch the nodes
-xterm  -e "export TURTLEBOT_GAZEBO_WORLD_FILE="$(pwd)/src/world/ahemd__world.world"; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e "roslaunch turtlebot_gazebo turtlebot_world.launch  world_file:=/home/workspace/catkin_ws/src/world/ahemd__world.world" &
 sleep 7
-xterm  -e "export TURTLEBOT_GAZEBO_MAP_FILE="$(pwd)/src/world/map.yaml"; roslaunch turtlebot_gazebo amcl_demo.launch " &
+xterm  -e "roslaunch turtlebot_gazebo amcl_demo.launch  map_file:=/home/workspace/catkin_ws/src/world/map.yaml" &
 sleep 5
 xterm  -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
