79c4ca02f54c1ff8eae5daab249169155d5fd69c
AhmedRadwan93 <ahmedelmaghwry93@gmail.com>
2021-01-03 12:25:17 +0000

    modifiying the wall follower script

--- src/scripts/wall_follower.sh
+++ src/scripts/wall_follower.sh
@@ -5,7 +5,7 @@
 # Launch the nodes
 xterm  -e " export TURTLEBOT_GAZEBO_WORLD_FILE="$(rospack find add_markers)/world/ahmed__world.world"; roslaunch turtlebot_gazebo turtlebot_world.launch  " &
 sleep 10
-xterm  -e "roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:="$(pwd)/src/slam_gmapping/gmapping/launch/slam_gmapping_pr2.launch"  " &
+xterm  -e "roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:="$(rospack find slam_gmapping)/gmapping/launch/slam_gmapping_pr2.launch"  " &
 sleep 5
 xterm  -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
